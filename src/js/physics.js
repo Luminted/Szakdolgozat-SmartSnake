534c2d4b7c6f1d2906bdd1c0b352920e9b64149f
rozsenich_levente <Levente.Rozsenich@innodox.com>
2017-09-28 21:31:41 +0200

    implementing observer pattern in physics engine

--- src/js/physics.js
+++ src/js/physics.js
@@ -10,48 +10,55 @@ export default class Physics extends Subject {
 
         this.observers = [];
         this.callbacks = callbacks;
+
+        this.subscibe = this.subscibe.bind(this);
     }
 
-    update(){
+    update() {
         this.calculateStepCollisionType();
     }
 
     calculateStepCollisionType() {
         const snake = this.callbacks.getEntityList().snake;
         const board = this.callbacks.getEntityList().board;
+        const pill = this.callbacks.getEntityList().pill;
         const snakeDirection = snake.direction;
         const velocity = snake.velocity;
-        const stepsTo = snake.calculateNextHead(velocity.X, velocity.Y);
+        const stepsTo = snake.calculateNextHead(velocity.posX, velocity.posY);
 
         //TODO maybe board returns wall objects if position is out of bounds?
-        if (stepsTo.posX < 0 || stepsTo.posY > board.state.width || stepsTo.posY < 0 || stepsTo.X > board.state.length) {
+        if (stepsTo.posX < 0 || stepsTo.posY > board.state.width || stepsTo.posY < 0 || stepsTo.posX > board.state.length) {
+            log.info('<---------------WALL_COLLISION_ACITON--------------->');
             for (let observer of this.observers) {
                 observer.onNotify(undefined, {
-                    message: "HELLO!"
-                })
+                    type: "WALL_COLLISION"
+                });
             }
         }
 
-        if (board.getTileByPosition(stepsTo.posX, stepsTo.posY).status === 'PILL') {
+        if (pill.position.posX === stepsTo.posX && pill.position.posY === stepsTo.posY) {
+            log.info('<---------------PILL_COLLISION_ACITON--------------->');
             for (let observer of this.observers) {
-                observer.onNotify(undefined, {
-                    message: "OLLEH!"
-                })
+                observer.onNotify(pill, {
+                    type: 'PILL_COLLISION',
+                    nourishment: this.callbacks.getEntityList().pill.nourishment
+                });
             }
         }
 
         for (let node of snake.state.body) {
             if (stepsTo.posX === node.posX && stepsTo.posY === node.posY) {
+                log.info('<---------------BODY_COLLISION_ACITON--------------->');
                 for (let observer of this.observers) {
-                    observer.onNotify(undefined, {
-                        message: "WORLD!"
-                    })
+                    observer.onNotify(node, {
+                        type: 'BODY_COLLISION_ACTION'
+                    });
                 }
             }
         }
     }
 
-    addObserver(observer) {
+    subscibe(observer) {
         this.observers.push(observer);
     }
 
