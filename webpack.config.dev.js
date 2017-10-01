f301df7d24743a25ceac6f9c8ca66818e743b3ef
rozsenich_levente <Levente.Rozsenich@innodox.com>
2017-10-01 18:52:36 +0200

    adding favicon with webpack plugins

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
     entry: './src/js/main.js',
@@ -27,10 +28,37 @@ module.exports = {
         }],
     },
     plugins: [
+        new FaviconsWebpackPlugin({
+            // Your source logo
+            logo: './src/dist/favicon.png',
+            // The prefix for all image files (might be a folder or a name)
+            prefix: 'icons-[hash]/',
+            // The name of the json containing all favicon information
+            statsFilename: 'iconstats-[hash].json',
+            // Generate a cache file with control hashes and
+            // don't rebuild the favicons until those hashes change
+            persistentCache: true,
+            // Inject the html into the html-webpack-plugin
+            inject: true,
+
+            // which icons should be generated (see https://github.com/haydenbleasel/favicons#usage)
+            icons: {
+                android: true,
+                appleIcon: true,
+                appleStartup: true,
+                coast: false,
+                favicons: true,
+                firefox: true,
+                opengraph: false,
+                twitter: false,
+                yandex: false,
+                windows: false
+            }
+        }),
         new HtmlWebpackPlugin({
             title: 'Snake.io',
             template: 'src/dist/index.html',
-            favicon: 'src/dist/favicon.ico'}),
+        }),
         new webpack.optimize.ModuleConcatenationPlugin(),
     ]
 };
