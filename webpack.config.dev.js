e594e6b3488a04919418ce6b7fd48f3ede19919e
rozsenich_levente <Levente.Rozsenich@innodox.com>
2017-10-18 14:54:20 +0200

    minor config changes due to package installing and removing

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -22,7 +22,8 @@ module.exports = {
             use: {
                 loader: 'babel-loader',
                 options: {
-                    presets: ['env']
+                    presets: ['env'],
+                    plugins: [require('babel-plugin-transform-object-rest-spread')]
                 }
             }
         }],
