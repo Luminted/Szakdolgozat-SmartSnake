d3e79fc5163cf790613fd6ac2908b6ee3a386936
Levente Rozsenich <rr.levente@gmail.com>
2019-04-29 15:57:07 +0200

    adding IntCoordinate class with tests and errors

---
+++ src/js/intCoordinate.js
@@ -0,0 +1,26 @@
+import IntCoordinateError from './errors/IntCoordinateError.js';
+
+export default class IntCoordinate {
+    constructor(x, y, nullPosition = false) {
+        if (Number.isInteger(x) && Number.isInteger(y) || nullPosition == true) {
+            this.x = Number(x);
+            this.y = Number(y);
+            this.nullPosition = nullPosition;
+
+        } else {
+            throw new IntCoordinateError('Coordinates have to be integer values!');
+        }
+    }
+
+    get coordinates() {
+
+        if (this.nullPosition) {
+            return undefined;
+        } else {
+            return{
+                x: this.x,
+                y: this.y
+            }
+        }
+    }
+}
