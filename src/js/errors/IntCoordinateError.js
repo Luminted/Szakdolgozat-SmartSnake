ac64bc65e2beeddc1ea3d4c1a079e8d95433578e
Levente Rozsenich <rr.levente@gmail.com>
2019-09-06 20:37:16 +0200

    scraping project

--- src/js/errors/IntCoordinateError.js
+++
@@ -1,6 +0,0 @@
-export default class CoordinateError extends Error{
-    constructor(message){
-        super(message);
-        this.name = 'IntCoordinateError';
-    }
-}
--- src/js/pathfinding-algorithms/index.js
+++
@@ -1,7 +0,0 @@
-import {squaredEuclideanDistance} from '../customUtils';
-import {ManhattanDistanceHeurism} from '../customUtils';
-
-export default {
-    EucledianSquared: squaredEuclideanDistance,
-    ManhattanHeurism: ManhattanDistanceHeurism
-}
